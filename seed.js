e88af30331760054c752a53836e1b4c0c8699df2
dimasarfan01 <77246142+dimasarfan01@users.noreply.github.com>
2021-03-15 01:24:19 +0700

    deploy heroku

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 // Connect to MongoDB via Mongoose
 // mongodb+srv://codeathome:bwamern@cluster0-40j6e.mongodb.net/db_staycation?retryWrites=true&w=majority
-seeder.connect('mongodb://127.0.0.1:27017/db_staycation', {
+seeder.connect('mongodb+srv://dimasarfan01:dimas12345@cluster0.21c2h.mongodb.net/db_staycation?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: true,
